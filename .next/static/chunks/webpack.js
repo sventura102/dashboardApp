8bd05fece91cf9205335aa9d065d28ba7aa16897
Stephanie <sventura102@gmail.com>
2024-11-25 17:54:19 -0800

    dashboard page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -89,6 +89,18 @@
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	(() => {
 /******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
@@ -178,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("249fc93f96430344")
+/******/ 		__webpack_require__.h = () => ("d658e98878a5242b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
